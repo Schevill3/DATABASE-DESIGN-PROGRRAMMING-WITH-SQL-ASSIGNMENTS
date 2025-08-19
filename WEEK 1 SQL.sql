4f58ceeb32d847fd887f8c7265ed58883d6ed83c
Schevill  Mwakio <schevilllynder@gmail.com>
2025-08-19 12:11:46 +0300

    Add files via upload

---
+++ WEEK 1 SQL.sql
@@ -0,0 +1,8 @@
+-- answers.sql
+-- Assignment: Introduction to Databases
+
+-- 1. Create a new database called salesDB
+CREATE DATABASE salesDB;
+
+-- 2. Drop (delete) the database called demo
+DROP DATABASE demo;
